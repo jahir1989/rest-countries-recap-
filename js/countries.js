79d52a6f7c1b50f48300b624007156386a62ab6b
jahir1989 <jahirul-jahir@gmail.com>
2023-03-20 21:11:45 +0600

    all countries api added

---
+++ js/countries.js
@@ -0,0 +1,28 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data)) 
+
+}
+
+const displayCountries = countries => {
+    // console.log(countries);
+    const countriesHtml = countries.map(country => getCountryHtml(country))
+    // console.log(countriesHtml);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHtml.join(' ');
+
+} 
+
+const getCountryHtml = country => {
+    return`
+        <div>
+            <h2>${country.name.common}<h2/>
+            <img src = "${country.flags.png}"
+        </div>
+    
+    
+    `
+}
+
+loadCountries();
